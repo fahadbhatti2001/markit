433fc1a5f8175718a2f4fc89ca9619ff484b3250
Fahad Bhatti <fahadqayyumbhatti2001@gmail.com>
2023-05-21 22:29:25 +0500

    Change API route

--- src/api/post.jsx
+++ src/api/post.jsx
@@ -1,17 +1,17 @@
-export const sendImages = async (data) => {
-    fetch('http://127.0.0.1:5000/process-images', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            return data.status
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
+// export const sendImages = async (data) => {
+//     fetch('http://127.0.0.1:5000/process-images', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(data),
+//     })
+//         .then((response) => response.json())
+//         .then((data) => {
+//             return data.status
+//         })
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         });
+// }
 
 // export const deleteImages = (id) => {
 //     const data = { id: id };
@@ -31,7 +31,7 @@ export const sendImages = async (data) => {
 
 export const recognizeImage = (image) => {
     const data = { image: image };
-    fetch('http://127.0.0.1:5000/recognize', {
+    fetch('http://192.168.10.2:5000/recognize', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
